a7561f87e98d03033018f1e0db38d88083d6bae1
legacychurchri <notavaiblee@gmail.com>
2020-09-26 01:14:57 -0400

    Initial commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import answersReducer from "hooks/useAnswers/reducer";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    answers: answersReducer,
   },
 });
 
